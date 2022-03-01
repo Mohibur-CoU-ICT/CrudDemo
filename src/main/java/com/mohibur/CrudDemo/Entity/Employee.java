862e8ecb7c66ab5bb17bf0904c85fe46c848484a
Mohibur Rahman <mohiburrahman5509@gmail.com>
2022-03-01 18:45:01 +0600

    Lombok dependancy added.

--- src/main/java/com/mohibur/CrudDemo/Entity/Employee.java
+++ src/main/java/com/mohibur/CrudDemo/Entity/Employee.java
@@ -1,8 +1,11 @@
 package com.mohibur.CrudDemo.Entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
+@Data
 @Entity
 public class Employee {
 
@@ -24,64 +27,4 @@ public class Employee {
 
     @Column
     private LocalDate dateOfBirth;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public long getSalary() {
-        return salary;
-    }
-
-    public void setSalary(long salary) {
-        this.salary = salary;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public Employee() {
-    }
-
-    public Employee(long id, String name, String department, String address, long salary, LocalDate date_of_birth) {
-        this.id = id;
-        this.name = name;
-        this.department = department;
-        this.address = address;
-        this.salary = salary;
-        this.dateOfBirth = date_of_birth;
-    }
 }
